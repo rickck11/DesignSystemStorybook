d1c32768fab92aee03374ca2cecabecbe2c3a2f3
rickck11 <rick32377@gmail.com>
2023-01-18 11:39:59 -0300

    DesignSystemStorybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/DesignSystemStorybook/'
     }
 
     return config
